2dc8e4e12a3f7d8b3e293c19ae0d35a42bf13615
Zsolt Tamás <tzsolt92@gmail.com>
2016-05-23 14:27:47 +0200

    Made a function that can calculate the intersection of 2 lists. Also made the random generated lists.

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,10 +1,26 @@
+import random
+
+
 def listoverlap(list1, list2):
-    return
+    l1 = set(list1)
+    l2 = set(list2)
+    l3 = l1 & l2
+    l4 = list(l3)
+    print(l4)
+    return l4
 
 
 def main():
+    list1 = random.sample(range(1, 100), 5)
+    print(list1)
+    list2 = random.sample(range(1, 100), 5)
+    print(list2)
+    listoverlap(list1, list2)
     return
 
 
 if __name__ == '__main__':
     main()
+
+# >>> import random
+# >>> random.sample(range(1, 100), 3)
