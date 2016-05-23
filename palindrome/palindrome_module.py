4f749b576bff622df9313488a4ea536ba2aa9324
Zsolt Tamás <tzsolt92@gmail.com>
2016-05-23 14:06:02 +0200

    Made a successful palindrome testing function.

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,24 @@
-def palindrome(str):
-    return
+def palindrome(string):
+    i = 0
+    boolean = False
+    string = str.lower(string)
+    str2 = list(string)
+    str2[:] = (value for value in str2 if value != " ")
+    while i < len(str2):
+        if str2[i] == str2[-i-1]:
+            i += 1
+            if i == len(str2):
+                boolean = True
+                print(boolean)
+        else:
+            print(boolean)
+            break
+    return boolean
 
 
 def main():
+    string = input("Add a string: ")
+    palindrome(string)
     return
 
 
